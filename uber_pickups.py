5ad9bcfcef50744dcf84db669c46183a2894235c
YukiYamamoto0222 <shinki.y.yamamoto@gmail.com>
2022-03-09 14:03:17 +0900

    my first streamlit app

---
+++ uber_pickups.py
@@ -0,0 +1,40 @@
+from locale import DAY_1
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('ニューヨークにおけるウーバー乗車地点のデータ')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache
+def load_data(nrows):
+  data = pd.read_csv(DATA_URL, nrows=nrows)
+  lowercase = lambda x: str(x).lower()
+  data.rename(lowercase, axis='columns', inplace=True)
+  data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+  return data
+
+data_load_state = st.text('データロード中...')
+
+data = load_data(10000)
+
+data_load_state.text('ロード完了 (@st.cache使用中)')
+
+if st.checkbox('生データを表示する'):
+  st.subheader('生データ')
+  st.write(data)
+
+st.subheader('時間帯ごとの全乗客数')
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+st.subheader('全ての乗車地点')
+st.map(data)
+
+hour_to_filter = st.slider('hour' ,0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'{hour_to_filter}時における乗車地点')
+st.map(filtered_data)
